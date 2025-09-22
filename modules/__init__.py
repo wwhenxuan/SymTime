758d75c66e8f5574162c4a4d1f7b60ca6083c88b
wwhenxuan <wwhenxuan@gmail.com>
2025-09-22 21:17:12 +0800

    whenxuan: init

--- modules/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on 2024/9/30 17:05
-@author: Whenxuan Wang
-@email: wwhenxuan@gmail.com
-"""
-from .pretrain_model import SymTime as SymTime_pretrain
-from .finetune_model import SymTime as SymTime_finetune
