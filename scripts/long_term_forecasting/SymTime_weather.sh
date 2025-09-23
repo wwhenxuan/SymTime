e66f0c2f9428af9caaaf7774cf165303aad5a7a8
wwhenxuan <wwhenxuan@gmail.com>
2025-09-23 10:52:26 +0800

    whenxuan: update the scripts for weather

--- scripts/long_term_forecasting/SymTime_weather.sh
+++ scripts/long_term_forecasting/SymTime_weather.sh
@@ -1,32 +1,80 @@
 export CUDA_VISIBLE_DEVICES=0
 
-for lradj in "type1" "type2" "cosine"
-do
-  for lr in 0.0001 0.00025
-  do
-    for batch_size in 16 8 4
-    do
-      for stride in 4 8
-      do
-        python -u long_term_forecast.py \
-          --task_name "long_term_forecast" \
-          --dataset_name "weather" \
-          --data "custom" \
-          --root_path "./datasets/weather/" \
-          --data_path "weather.csv" \
-          --seq_len 336 \
-          --pred_len 96 \
-          --forward_layers 3 \
-          --stride $stride \
-          --batch_size $batch_size \
-          --learning_rate $lr \
-          --lradj $lradj \
-          --num_workers 5 \
-          --enc_in 21 \
-          --train_epochs 13 \
-          --patience 3 \
 
-        done
-      done
-    done
-done
+seq_len = 336
+
+
+python -u long_term_forecast.py \
+  --task_name "long_term_forecast" \
+  --dataset_name "weather" \
+  --data "custom" \
+  --root_path "./datasets/weather/" \
+  --data_path "weather.csv" \
+  --seq_len $seq_len \
+  --pred_len 96 \
+  --forward_layers 3 \
+  --stride 4 \
+  --batch_size 16 \
+  --learning_rate 0.0001 \
+  --lradj "type2" \
+  --num_workers 5 \
+  --enc_in 21 \
+  --train_epochs 13 \
+  --patience 3 \
+
+
+python -u long_term_forecast.py \
+  --task_name "long_term_forecast" \
+  --dataset_name "weather" \
+  --data "custom" \
+  --root_path "./datasets/weather/" \
+  --data_path "weather.csv" \
+  --seq_len $seq_len \
+  --pred_len 192 \
+  --forward_layers 3 \
+  --stride 8 \
+  --batch_size 16 \
+  --learning_rate 0.0001 \
+  --lradj "type2" \
+  --num_workers 5 \
+  --enc_in 21 \
+  --train_epochs 13 \
+  --patience 3 \
+
+
+python -u long_term_forecast.py \
+  --task_name "long_term_forecast" \
+  --dataset_name "weather" \
+  --data "custom" \
+  --root_path "./datasets/weather/" \
+  --data_path "weather.csv" \
+  --seq_len $seq_len \
+  --pred_len 336 \
+  --forward_layers 3 \
+  --stride 8 \
+  --batch_size 16 \
+  --learning_rate 0.0001 \
+  --lradj "type2" \
+  --num_workers 5 \
+  --enc_in 21 \
+  --train_epochs 13 \
+  --patience 3 \
+
+
+python -u long_term_forecast.py \
+  --task_name "long_term_forecast" \
+  --dataset_name "weather" \
+  --data "custom" \
+  --root_path "./datasets/weather/" \
+  --data_path "weather.csv" \
+  --seq_len $seq_len \
+  --pred_len 720 \
+  --forward_layers 3 \
+  --stride 8 \
+  --batch_size 4 \
+  --learning_rate 0.0001 \
+  --lradj "type1" \
+  --num_workers 5 \
+  --enc_in 21 \
+  --train_epochs 13 \
+  --patience 3 \
