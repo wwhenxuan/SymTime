08a4f044d63708619ca3b81cc96c5e21ac411228
wwhenxuan <wwhenxuan@gmail.com>
2025-10-07 21:26:17 +0800

    whenxuan: update the scripts for pretraining

--- scripts/SymTime_pretrain.sh
+++ scripts/SymTime_pretrain.sh
@@ -1,8 +1,5 @@
 export CUDA_VISIBLE_DEVICES = 0 1 2 3 4 5 6 7
 
-accelerate launch pretrain.py
-
-
 accelerate launch --multi_gpu --mixed_precision=fp16 --num_processes=8  \ 
   pretrain.py \ 
     --model "base" \ 
